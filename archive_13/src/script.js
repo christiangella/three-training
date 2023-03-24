c8cb05861740f7547cdf7dbf5569146058477441
christiangella <112913805+christiangella@users.noreply.github.com>
2023-03-23 17:29:06 -0700

    building out gui debug panel

--- archive_13/src/script.js
+++ archive_13/src/script.js
@@ -24,6 +24,11 @@ const parameters = {
 
 parameters.count = 1000
 parameters.size = 0.02
+parameters.radius = 5
+parameters.branches = 3
+parameters.spin = 1
+parameters.randomness = 0.2
+parameters.randomnessPower = 3
 
 let geometry = null
 let material = null
@@ -44,9 +49,17 @@ const generateGalaxy = () => {
 
     for (let i = 0; i < parameters.count; i++) {
         const i3 = i * 3
-        positions[i3 + 0] = (Math.random() - 0.5) * 3
-        positions[i3 + 1] = (Math.random() - 0.5) * 3
-        positions[i3 + 2] = (Math.random() - 0.5) * 3
+
+        const radius = Math.random() * parameters.radius
+        const spinAngle = radius * parameters.spin
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness
+        positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX
+        positions[i3 + 1] = randomY
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
     }
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
 
@@ -64,8 +77,13 @@ const generateGalaxy = () => {
 
 generateGalaxy()
 
-gui.add(parameters, 'count').min(50).max(2500).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'count').min(50).max(25000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'size').min(0.001).max(0.04).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin').min(-5).max(1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
 
 /**
  * Sizes
