6ee079cafeebef13a7e6d04d9a8f6a78365508df
christiangella <112913805+christiangella@users.noreply.github.com>
2023-03-25 22:16:30 -0700

    initializing sphere shape

--- archive_15/src/script.js
+++ archive_15/src/script.js
@@ -32,6 +32,19 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/nz.png'
 ])
 
+/** PHYSICS */
+/** world */
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
+
+/** sphere */
+const sphereShape = new CANNON.Sphere(0.5)
+const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    sphere: sphereShape
+})
+
 /**
  * Test sphere
  */
